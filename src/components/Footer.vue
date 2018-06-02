4fe004a1af77a670a7d8d3115e42f969ae69cac7
webarata3 <webarata3@gmail.com>
2018-06-02 10:36:50 +0900

    不要コンポーネントの削除

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
-  <footer>©2018 Shinichi ARATA（webarata3）</footer>
+  <footer>
+    <div class="copyright">©2018 Shinichi ARATA（webarata3）</div>
+    <div class="sns">
+      <ul>
+        <li><a href="https://twitter.com/webarata3"><span class="fa fa-twitter"></span></a></li>
+        <li><a href="https://facebook.com/arata.shinichi"><span class="fa fa-facebook"></span></a></li>
+        <li><a href="https://github.com/webarata3"><span class="fa fa-github"></span></a></li>
+        <li><a href="https://ja.stackoverflow.com/users/2214/webarata3?tab=profile"><span
+          class="fa fa-stack-overflow"></span></a></li>
+      </ul>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -13,7 +24,25 @@ export default {
 
 <style>
   footer {
+    display: flex;
+    flex-direction: row;
     font-size: 14px;
     padding: 5px;
+  }
+
+  .copyright {
+    flex: 1;
+  }
+
+  ul {
+    display: flex;
+  }
+
+  li {
+    margin-right: 5px;
+  }
+
+  a, a:hover, a:active, a:visited {
+    color: black;
   }
 </style>
