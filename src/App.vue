e6bbda17bf6d2fb9da3aea45f9174acd0eecbd60
webarata3 <webarata3@gmail.com>
2018-06-09 08:08:48 +0900

    ぐるぐるを削除

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,6 @@
       <Menu></Menu>
     </header>
     <main>
-      <div id="loader" class="loader" v-cloak>
-        <svg viewBox="0 0 32 32" width="32" height="32">
-          <circle id="spinner" cx="16" cy="16" r="14" fill="none"></circle>
-        </svg>
-      </div>
       <div class="region" v-cloak>
         <div class="select-region">
           <label for="region">地域</label>
@@ -240,75 +235,5 @@
 
   select {
     flex: 1;
-  }
-
-  /*
-     アニメーション
-     https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/?hl=ja
-    */
-  .loader {
-    left: 50%;
-    top: 50%;
-    position: fixed;
-    transform: translate(-50%, -50%);
-  }
-
-  .loader #spinner {
-    box-sizing: border-box;
-    stroke: #417ab7;
-    stroke-width: 3px;
-    transform-origin: 50%;
-    animation: line 1.6s cubic-bezier(0.4, 0, 0.2, 1) infinite,
-    rotate 1.6s linear infinite;
-  }
-
-  @-webkit-keyframes rotate {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(450deg);
-    }
-  }
-
-  @keyframes rotate {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(450deg);
-    }
-  }
-
-  @-webkit-keyframes line {
-    0% {
-      stroke-dasharray: 2, 85.964;
-      transform: rotate(0);
-    }
-    50% {
-      stroke-dasharray: 65.973, 21.9911;
-      stroke-dashoffset: 0;
-    }
-    100% {
-      stroke-dasharray: 2, 85.964;
-      stroke-dashoffset: -65.973;
-      transform: rotate(90deg);
-    }
-  }
-
-  @keyframes line {
-    0% {
-      stroke-dasharray: 2, 85.964;
-      transform: rotate(0);
-    }
-    50% {
-      stroke-dasharray: 65.973, 21.9911;
-      stroke-dashoffset: 0;
-    }
-    100% {
-      stroke-dasharray: 2, 85.964;
-      stroke-dashoffset: -65.973;
-      transform: rotate(90deg);
-    }
   }
 </style>
